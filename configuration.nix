8a67cbdd5b5e9b61a0cd290052abcad63fa08b94
Weegs710 <weegs@tutamail.com>
2025-06-26 22:22:55 -0400

    removed ly, added and enabled cosmic greeter and xwayland for cosmic, disabled sddm

--- configuration.nix
+++ configuration.nix
@@ -60,9 +60,10 @@
   services.xserver.enable = true;
 
   # Enable the KDE Plasma Desktop Environment.
-  services.displayManager.sddm.enable = true;
-  services.displayManager.ly.enable = false;
+  services.displayManager.sddm.enable = false;
+  services.displayManager.cosmic-greeter.enable = true;
   services.desktopManager.cosmic.enable = true;
+  services.desktopManager.cosmic.xwayland.enable = true;
   # Configure keymap in X11
   services.xserver.xkb = {
     layout = "us";
