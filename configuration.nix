420cccec2752e7d521f1d991de5625ebea9706f1
Weegs710 <weegs@tutamail.com>
2025-06-30 11:35:10 -0400

    disabled firewall again, everything is way too slow, look into this later

--- configuration.nix
+++ configuration.nix
@@ -237,7 +237,7 @@
   # networking.firewall.allowedTCPPorts = [ ... ];
   # networking.firewall.allowedUDPPorts = [ ... ];
   # Or disable the firewall altogether.
-  networking.firewall.enable = true;
+  networking.firewall.enable = false;
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
