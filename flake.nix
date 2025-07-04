15126457552289380a29eb1d743409e814d68593
Weegs710 <weegs@tutamail.com>
2025-07-03 22:34:53 -0400

    updated flake input url to proper release instead of just blank

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
     nix-flatpak.url = "github:gmodena/nix-flatpak"; # unstable branch. Use github:gmodena/nix-flatpak/?ref=<tag> to pin releases.
-    stylix.url = "github:danth/stylix";
+    stylix.url = "github:danth/stylix/release-25.05";
   };
 
   outputs = {
