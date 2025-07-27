d8b65044ca1928c7a431bc08c3721290a71673b4
Weegs710 <weegs@tutamail.com>
2025-07-26 23:08:02 -0400

    refactored packages, separated system and user packages, as well as migrated some packages to home manager

--- home.nix
+++ home.nix
@@ -23,6 +23,19 @@
   # The home.packages option allows you to install Nix packages into your
   # environment.
   home.packages = [
+    alejandra
+    btop-rocm
+    cliphist
+    dunst
+    fastfetch
+    fzf
+    hyprshot
+    jq
+    rofi
+    swww
+    tldr
+    wlogout
+    wlsunset
     # # Adds the 'hello' command to your environment. It prints a friendly
     # # "Hello, world!" when run.
     # pkgs.hello
