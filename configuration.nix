f7a2a92b990aba39a4bc75fa6f60171803a26ba6
Weegs710 <weegs@tutamail.com>
2025-07-27 21:14:09 -0400

    added cachix support and used cachix for hyprland. ran update-all to ensure i was no longer compiling hyprland.

--- configuration.nix
+++ configuration.nix
@@ -72,6 +72,14 @@ in
       "nix-command"
       "flakes"
     ];
+    substituters = [
+      "https://nix-community.cachix.org"
+      "https://hyprland.cachix.org"
+    ];
+    trusted-public-keys = [
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+    ];
   };
 
   # Home Manager Settings.
