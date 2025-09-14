97719af805ea2a5adb6d0da31b094f6dd8f13438
Weegs710 <weegs@tutamail.com>
2025-09-14 12:26:44 -0400

    migrated waybar config and style to home manager and use stylix for base16 colors. IMPORTANT NOTE: html values in config do not accept base values, proper hex values will need to be put in place for proper themes

--- home.nix
+++ home.nix
@@ -54,6 +54,246 @@
 
   programs.home-manager.enable = true;
 
+  programs.waybar = {
+    enable = true;
+    settings = {
+      mainBar = {
+        layer = "bottom";
+        position = "top";
+        height = 36;
+        spacing = 0;
+        modules-left = [
+          "tray"
+          "hyprland/workspaces"
+        ];
+        modules-center = [
+          "hyprland/window"
+        ];
+        modules-right = [
+          "network"
+          "custom/temperature"
+          "bluetooth"
+          "pulseaudio"
+          "clock"
+          "custom/lock"
+          "custom/reboot"
+          "custom/power"
+        ];
+        "hyprland/workspaces" = {
+          disable-scroll = false;
+          all-outputs = true;
+          format = "{icon}";
+          on-click = "activate";
+          persistent-workspaces = {
+            "*" = [ 1 ];
+          };
+          format-icons = {
+            "1" = " ";
+          };
+        };
+        "custom/lock" = {
+          format = "<span color='#7dcfff'>  </span>";
+          on-click = "hyprlock";
+          tooltip = true;
+          tooltip-format = "Lock";
+        };
+        "custom/reboot" = {
+          format = "<span color='#7dcfff'>  </span>";
+          on-click = "systemctl reboot";
+          tooltip = true;
+          tooltip-format = "Reboot";
+        };
+        "custom/power" = {
+          format = "<span color='#b53dff'> ⏻ </span>";
+          on-click = "systemctl poweroff";
+          tooltip = true;
+          tooltip-format = "Power Off";
+        };
+        network = {
+          format-wifi = "<span color='#7dcfff'> 󰤨  </span>{essid} ";
+          format-ethernet = "<span color='#53b397'>   </span>Wired ";
+          tooltip-format = "<span color='#b53dff'> 󰅧  </span>{bandwidthUpBytes}  <span color='#5ca8dc'> 󰅢 </span>{bandwidthDownBytes}";
+          format-linked = "<span color='#2082a6'> 󱘖  </span>{ifname} (No IP) ";
+          format-disconnected = "<span color='#b53dff'>   </span>Disconnected ";
+          format-alt = "<span color='#7dcfff'> 󰤨  </span>{signalStrength}% ";
+          interval = 1;
+          on-click-right = "kitty -e nmtui";
+        };
+        pulseaudio = {
+          format = "<span color='#53b397'>{icon}</span>{volume}% ";
+          format-muted = "<span color='#b53dff'>  </span>0% ";
+          format-icons = {
+            headphone = "<span color='#a175d4'>  </span>";
+            hands-free = "<span color='#a175d4'>  </span>";
+            headset = "<span color='#a175d4'>  </span>";
+            phone = "<span color='#7dcfff'>  </span>";
+            portable = "<span color='#7dcfff'>  </span>";
+            car = "<span color='#2082a6'>  </span>";
+            default = [
+              "<span color='#6b7394'>  </span>"
+              "<span color='#7dcfff'>  </span>"
+              "<span color='#53b397'>  </span>"
+            ];
+          };
+          on-click-right = "pavucontrol -t 3";
+          on-click = "pactl -- set-sink-mute 0 toggle";
+          tooltip = true;
+          tooltip-format = "Volume: {volume}%";
+        };
+        "custom/temperature" = {
+          exec = "/run/current-system/sw/bin/sensors | /run/current-system/sw/bin/awk '/edge:/ {gsub(/[+°C]/, \"\", $2); print int($2); exit}'";
+          format = "<span color='#2082a6'>  </span>{}°C ";
+          interval = 5;
+          tooltip = true;
+          tooltip-format = "Current CPU Temperature:  {}°C";
+        };
+        memory = {
+          format = "<span color='#a175d4'>   </span>{used:0.1f}GB ";
+          tooltip = true;
+          tooltip-format = "RAM Usage: {used:0.2f}GB/{total:0.2f}GB";
+        };
+        cpu = {
+          format = "<span color='#2082a6'>   </span>{usage}% ";
+          tooltip = true;
+        };
+        clock = {
+          interval = 1;
+          format = "<span color='#a175d4'> 󰃰 </span>{:%X} ";
+          tooltip = true;
+          tooltip-format = "{:L%A %m/%d/%Y}";
+        };
+        tray = {
+          icon-size = 24;
+          spacing = 6;
+        };
+        bluetooth = {
+          format = "<span color='#5ca8dc'>  </span>{status} ";
+          format-connected = "<span color='#5ca8dc'>ᛒ</span>{device_alias} ";
+          format-connected-battery = "<span color='#5ca8dc'>ᛒ</span>{device_alias} {device_battery_percentage}% ";
+          tooltip-format = "{controller_alias}\t{controller_address}\n\n{num_connections} connected";
+          tooltip-format-connected = "{controller_alias}\t{controller_address}\n\n{num_connections} connected\n\n{device_enumerate}";
+          tooltip-format-enumerate-connected = "{device_alias}\t{device_address}";
+          tooltip-format-enumerate-connected-battery = "{device_alias}\t{device_address}\t{device_battery_percentage}%";
+          on-click-right = "kitty -e bluetui";
+        };
+      };
+    };
+    style = lib.mkAfter ''
+      * {
+          font-family: "Terminess Nerd Font";
+          font-weight: bold;
+          font-size: 18px;
+          color: @base05;
+      }
+
+      #tray menu {
+          background-color: @base01;
+      }
+
+      window#waybar {
+          background: transparent;
+      }
+
+      #waybar {
+          background: transparent;
+          border: none;
+      }
+
+      #workspaces,
+      #window,
+      #tray {
+          background: alpha(@base00, 0.5);
+          padding: 4px 6px;
+          margin-top: 6px;
+          margin-left: 6px;
+          margin-right: 6px;
+          border-radius: 10px;
+      }
+
+      #clock,
+      #custom-power {
+          background: alpha(@base00, 0.5);
+          margin-top: 6px;
+          margin-right: 6px;
+          padding: 4px 2px;
+          border-radius: 0 10px 10px 0;
+      }
+
+      #network,
+      #custom-lock {
+          background: alpha(@base00, 0.5);
+          margin-top: 6px;
+          margin-left: 6px;
+          padding: 4px 2px;
+          border-radius: 10px 0 0 10px;
+      }
+
+      #custom-reboot,
+      #bluetooth,
+      #battery,
+      #pulseaudio,
+      #backlight,
+      #custom-temperature,
+      #memory,
+      #cpu {
+          background: alpha(@base00, 0.5);
+          margin-top: 6px;
+          padding: 4px 2px;
+      }
+
+      #custom-temperature.critical,
+      #pulseaudio.muted {
+          color: @base08;
+          padding-top: 0;
+      }
+
+      #bluetooth:hover,
+      #network:hover,
+      #backlight:hover,
+      #battery:hover,
+      #pulseaudio:hover,
+      #custom-temperature:hover,
+      #memory:hover,
+      #cpu:hover,
+      #clock:hover,
+      #custom-lock:hover,
+      #custom-reboot:hover,
+      #custom-power:hover,
+      #window:hover {
+          background: alpha(@base03, 0.5);
+      }
+
+      #workspaces button:hover {
+          background: alpha(@base0D, 0.2);
+          padding: 2px 8px;
+          margin: 0 2px;
+          border-radius: 10px;
+      }
+
+      #workspaces button.active {
+          background: @base0D;
+          color: @base05;
+          padding: 2px 8px;
+          margin: 0 2px;
+          border-radius: 10px;
+      }
+
+      #workspaces button {
+          background: transparent;
+          border: none;
+          color: @base03;
+          padding: 2px 8px;
+          margin: 0 2px;
+          font-weight: bold;
+      }
+
+      #window {
+          font-weight: 500;
+          font-style: italic;
+      }
+    '';
+  };
+
   wayland.windowManager.hyprland = {
     enable = true;
     settings = {
