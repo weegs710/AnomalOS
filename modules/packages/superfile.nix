9b9f29e68eecf14443785691796f30c8e78ea493
Weegs710 <weegs@tutamail.com>
2026-01-29 03:36:37 -0500

    feat: add wrapped packages for oh-my-posh, zed, and superfile

---
+++ modules/packages/superfile.nix
@@ -0,0 +1,36 @@
+# Wrapped superfile with config and tools
+# Run with: nix run github:weegs710/AnomalOS#superfile
+{ ... }: {
+  perSystem = { pkgs, ... }:
+    let
+      # Pinned version
+      superfilePkg = pkgs.superfile.overrideAttrs (old: {
+        version = "1.5.0";
+        src = pkgs.fetchFromGitHub {
+          owner = "yorukot";
+          repo = "superfile";
+          rev = "v1.5.0";
+          hash = "sha256-PEojifuiIjF3OUxDoMCyynOJUpFglTzh7lJUcq7g4e0=";
+        };
+        vendorHash = "sha256-5SSnrG3DvD1i7rNcpztHkUUap4Qp7MX04ofD7rA3xgM=";
+        doCheck = false;
+      });
+
+      extraTools = with pkgs; [ exiftool zoxide ];
+
+      wrappedSuperfile = pkgs.symlinkJoin {
+        name = "superfile-wrapped";
+        paths = [ superfilePkg ];
+        buildInputs = extraTools;
+        nativeBuildInputs = [ pkgs.makeWrapper ];
+        postBuild = ''
+          wrapProgram $out/bin/superfile \
+            --prefix PATH : ${pkgs.lib.makeBinPath extraTools}
+        '';
+        meta.mainProgram = "superfile";
+      };
+    in
+    {
+      packages.superfile = wrappedSuperfile;
+    };
+}
