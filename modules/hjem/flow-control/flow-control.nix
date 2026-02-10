a71b3ea981576c270d4ab3204ce535391300a2ae
weegs710 <weegs@tutamail.com>
2026-02-10 16:25:13 -0500

    feat: add declarative flow-control configuration with persistent settings
    - Create custom_config with 120fps, theme, gutter, LSP, and editor preferences
    - Configure Hjem module to deploy custom_config via XDG config files
    - Update shareables wrapper to bundle config for nix run portability
    - Use include_files mechanism to prevent flow from overwriting custom settings

--- modules/hjem/flow-control/flow-control.nix
+++ modules/hjem/flow-control/flow-control.nix
@@ -7,10 +7,15 @@
   }: let
     username = config.mySystem.user.name;
     wrappedFlow = inputs.self.packages.${pkgs.stdenv.hostPlatform.system}.flow-control;
+    customConfigFile = ./custom_config;
   in
     with lib; {
       config = mkIf config.mySystem.features.development {
         users.users.${username}.packages = [wrappedFlow];
+
+        hjem.users.${username} = {
+          xdg.config.files."flow/custom_config".source = customConfigFile;
+        };
       };
     };
 }
--- modules/shareables/flow-control.nix
+++ modules/shareables/flow-control.nix
@@ -8,13 +8,36 @@
       ruff
     ];
 
+    customConfig = ../hjem/flow-control/custom_config;
+
+    flowWrapper = pkgs.writeShellScript "flow-wrapper" ''
+      FLOW_CONFIG_DIR="''${XDG_CONFIG_HOME:-$HOME/.config}/flow"
+      FLOW_CUSTOM_CONFIG="$FLOW_CONFIG_DIR/custom_config"
+      FLOW_MAIN_CONFIG="$FLOW_CONFIG_DIR/config"
+
+      mkdir -p "$FLOW_CONFIG_DIR"
+
+      if [ ! -f "$FLOW_CUSTOM_CONFIG" ]; then
+        cp ${customConfig} "$FLOW_CUSTOM_CONFIG"
+      fi
+
+      if [ ! -f "$FLOW_MAIN_CONFIG" ]; then
+        echo 'include_files "'"$FLOW_CUSTOM_CONFIG"'"' > "$FLOW_MAIN_CONFIG"
+      elif ! grep -q "include_files" "$FLOW_MAIN_CONFIG"; then
+        echo 'include_files "'"$FLOW_CUSTOM_CONFIG"'"' >> "$FLOW_MAIN_CONFIG"
+      fi
+
+      exec ${pkgs.flow-control}/bin/flow "$@"
+    '';
+
     wrappedFlow = pkgs.symlinkJoin {
       name = "flow-control-wrapped";
       paths = [pkgs.flow-control];
       buildInputs = lspTools;
       nativeBuildInputs = [pkgs.makeWrapper];
       postBuild = ''
-        wrapProgram $out/bin/flow \
+        rm $out/bin/flow
+        makeWrapper ${flowWrapper} $out/bin/flow \
           --prefix PATH : ${pkgs.lib.makeBinPath lspTools}
       '';
       meta.mainProgram = "flow";
