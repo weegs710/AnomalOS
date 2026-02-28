a0a65ca4f83e14a56654dd4886d750d5bbdb7d83
weegs710 <weegs@tutamail.com>
2026-02-27 21:47:23 -0500

    chore: updated portable fastfetch module

--- modules/shareables/fastfetch.nix
+++ modules/shareables/fastfetch.nix
@@ -1,103 +1,117 @@
 {
   perSystem = {pkgs, ...}: let
-    logo = ../hjem/fastfetch/nixos.png;
+    logo = ../hjem/fastfetch/fetch-logo.webp;
     configFile = pkgs.writeText "fastfetch-config.jsonc" ''
       {
         "$schema": "https://github.com/fastfetch-cli/fastfetch/raw/dev/doc/json_schema.json",
         "logo": {
           "source": "${logo}",
           "type": "kitty",
-          "width": 38,
-          "height": 19,
+          "width": 39,
+          "height": 20,
           "padding": {
+            "left": 2,
             "right": 2,
             "top": 2
           }
         },
         "display": {
-          "separator": " "
+          "separator": " 󰑃 "
         },
         "modules": [
-          "break",
           {
-            "type": "title",
-            "keyWidth": 10
+            "type": "title"
           },
-          "break",
+          {
+            "type": "separator"
+          },
+          {
+            "type": "custom",
+            "format": " SYSTEM"
+          },
           {
             "type": "os",
-            "key": " ",
-            "keyColor": "34"
+            "key": "│ ├"
           },
           {
             "type": "kernel",
-            "key": " ",
-            "keyColor": "34"
+            "key": "│ ├"
           },
           {
             "type": "command",
-            "key": "󰦛 ",
-            "keyColor": "34",
+            "key": "│ ├󰦛",
             "text": "readlink /nix/var/nix/profiles/system | sed -E 's/system-([0-9]+)-link/Generation: \\1/'"
           },
           {
             "type": "command",
-            "key": " ",
-            "keyColor": "34",
+            "key": "│ ├",
             "text": "nix path-info -S /run/current-system | awk '{printf \"Closure Size: %.2f GB\", $2/1024/1024/1024}'"
-          },
-          "break",
-          {
-            "type": "cpu",
-            "key": " ",
-            "keyColor": "blue"
-          },
-          {
-            "type": "gpu",
-            "key": "󰢮 ",
-            "keyColor": "blue"
-          },
-          {
-            "type": "memory",
-            "key": " ",
-            "keyColor": "blue"
-          },
-          "break",
-          {
-            "type": "wm",
-            "key": " ",
-            "keyColor": "34"
-          },
-          {
-            "type": "shell",
-            "key": " ",
-            "keyColor": "34"
-          },
-          "break",
-          {
-            "type": "uptime",
-            "key": " ",
-            "keyColor": "34",
-            "text": "Uptime: "
           },
           {
             "type": "command",
-            "key": " ",
-            "keyColor": "34",
-            "text": "birth_install=$(stat -c %W /); current=$(date +%s); time_progression=$((current - birth_install)); days_difference=$((time_progression / 86400)); echo $days_difference days since the last fresh install."
+            "key": "│ └",
+            "text": "birth_install=1750377600; current=$(date +%s); time_progression=$((current - birth_install)); days_difference=$((time_progression / 86400)); echo $days_difference days using NixOS"
           },
-          "break",
+          {
+            "type": "custom",
+            "format": " HARDWARE"
+          },
+          {
+            "type": "cpu",
+            "key": "│ ├"
+          },
+          {
+            "type": "gpu",
+            "key": "│ ├󰢮"
+          },
+          {
+            "type": "memory",
+            "key": "│ └"
+          },
+          {
+            "type": "custom",
+            "format": " DESKTOP"
+          },
+          {
+            "type": "wm",
+            "key": "│ ├"
+          },
+          {
+            "type": "shell",
+            "key": "│ ├"
+          },
+          {
+            "type": "terminal",
+            "key": "│ ├"
+          },
+          {
+            "type": "terminalfont",
+            "key": "│ └"
+          },
+          {
+            "type": "custom",
+            "format": "󰍹 MEDIA"
+          },
+          {
+            "type": "display",
+            "key": "│ ├󰍹"
+          },
+          {
+            "type": "sound",
+            "key": "│ ├󰓃"
+          },
+          {
+            "type": "player",
+            "key": "│ ├󰗜"
+          },
           {
             "type": "media",
-            "key": "󰝚 ",
-            "keyColor": "34"
+            "key": "│ └󰝚"
           },
-          "break",
           {
             "type": "custom",
             "format": "\u001b[90m  \u001b[31m  \u001b[32m  \u001b[33m  \u001b[34m  \u001b[35m  \u001b[36m  \u001b[37m "
-          },
-          "break"
+          }
         ]
       }
     '';
