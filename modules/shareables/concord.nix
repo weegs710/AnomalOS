483ee6964084905a5dda16f4259cf4c55d6e1629
weegs710 <weegs@tutamail.com>
2026-05-13 23:36:30 -0400

    chore(concord): drop concord-extras patch, update to latest upstream
    - remove modules/shareables/patches/concord-extras
    - update concord flake input fea0f6b9 (v1.3.3) -> 32349982 (2026-05-14)

--- modules/shareables/concord.nix
+++ modules/shareables/concord.nix
@@ -1,24 +1,5 @@
 { inputs, ... }: {
-  perSystem = { pkgs, lib, ... }: let
-    upstream = inputs.concord.packages.${pkgs.stdenv.hostPlatform.system}.default;
-
-    patched = upstream.overrideAttrs (old: {
-      patches = (old.patches or []) ++ [
-        ./patches/concord-extras
-      ];
-    });
-
-    concordWrapped = pkgs.symlinkJoin {
-      name = "concord";
-      paths = [ patched ];
-      nativeBuildInputs = [ pkgs.makeWrapper ];
-      postBuild = ''
-        wrapProgram $out/bin/concord \
-          --prefix PATH : ${lib.makeBinPath [ pkgs.wl-clipboard ]}
-      '';
-      meta = patched.meta // { mainProgram = "concord"; };
-    };
-  in {
-    packages.concord = concordWrapped;
+  perSystem = { system, ... }: {
+    packages.concord = inputs.concord.packages.${system}.default;
   };
 }
