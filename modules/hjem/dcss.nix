0e5b6b29419f9ea7d00ae80dfd16303715968ae3
weegs710 <weegs@tutamail.com>
2026-06-05 15:11:08 -0400

    feat(dcss): expand crawl init config with safety, alerts, and necromancer setup
    - add autoinscribe rules requiring confirmation on dangerous potions/scrolls (mutation, lignification, torment, amnesia, bad/dangerous items)
    - add force_more_message alerts for portal expiry warnings, dangerous monster arrivals, and status changes
    - add flash_screen_message alerts for confusion, slowing, grabs, and held states
    - add necromancer spell_slot letter assignments and number-key cast macros
    - add notes config (note_hp_percent, note_all_skill_levels, note_items, note_messages, note_monsters)
    - add autopickup_exceptions for dispersal, throwing net, and manual of
    - set fail_severity_to_confirm to 2, autofight_stop to 65, allow_self_target to no
    - add default_manual_training, explore_delay, show_more, sort_menus, dump_item_origins, dump_message_count

--- modules/hjem/dcss.nix
+++ modules/hjem/dcss.nix
@@ -16,6 +16,7 @@ in
     restart_after_game = true
     restart_after_save = false
     pregen_dungeon = full
+    default_manual_training = true
 
     # Autopickup
     autopickup = $?!+"/♦}/
@@ -24,11 +25,23 @@ in
     autopickup_exceptions += >scroll of immolation
     autopickup_exceptions += >scroll of inaccuracy
     autopickup_exceptions += <curare
+    autopickup_exceptions += <dispersal
+    autopickup_exceptions += <throwing net
+    autopickup_exceptions += <manual of
     assign_item_slot = backward
+
+    # Autoinscribe dangerous consumables to require confirmation
+    autoinscribe += potion of mutation:!q
+    autoinscribe += potion of lignification:!q
+    autoinscribe += scroll of torment:!r
+    autoinscribe += scroll of amnesia:!r
+    autoinscribe += (bad|dangerous)_item.*potion:!q
+    autoinscribe += (bad|dangerous)_item.*scroll:!r
 
     # Travel & Exploration
     travel_delay = 30
     rest_delay = -1
+    explore_delay = -1
     runrest_ignore_monster += bat:3
     rest_wait_both = true
     rest_wait_ancestor = true
@@ -41,10 +54,12 @@ in
     # Command Enhancements
     auto_switch = true
     spell_menu = true
-    fail_severity_to_confirm = -1
+    fail_severity_to_confirm = 2
     autofight_fires = true
     autofight_caught = true
     autofight_wait = true
+    autofight_stop = 65
+    allow_self_target = no
     warn_hatches = true
     single_column_item_menus = false
 
@@ -60,18 +75,82 @@ in
     view_delay = 200
     always_show_gems = true
     more_gem_info = true
+    show_more = false
+    sort_menus = true : equipped, art, ego, glowing, identified, basename, qualname, qty
+    dump_item_origins = all
+    dump_message_count = 100
+
+    # Notes (written to morgue file and readable via ?: in-game)
+    note_hp_percent = 10
+    note_all_skill_levels = true
+    note_items += acquirement, artefact
+    note_messages += You fall through a shaft
+    note_messages += [bB]anish.*Abyss
+    note_messages += You have reached level
+    note_monsters += ancient lich, orb of fire, curse skull, pandemonium lord
+
+    # Dangerous monster alerts
     force_more_message += You are sucked into a shaft
     force_more_message += You fall through a shaft
     force_more_message += You are engulfed in a sudden explosion
     force_more_message += The .* is revealed
     force_more_message += You feel a surge of divine power
     force_more_message += You are.* teleport
+    force_more_message += You have reached level
+    force_more_message += You feel yourself slow down
+    force_more_message += You are confused
+    force_more_message += You become entangled
+    force_more_message += .* grabs you
+    force_more_message += filled with .* inner flame
+    # Portal expiry warnings -- missing these = lost loot
+    force_more_message += ticking.*clock
+    force_more_message += dying ticks
+    force_more_message += distant snort
+    force_more_message += coins.*counted
+    force_more_message += tolling.*bell
+    force_more_message += roar of battle
+    force_more_message += creaking.*portcullis
+    force_more_message += portcullis is probably
+    force_more_message += wave of frost
+    force_more_message += hiss.*sand
+    force_more_message += sound.*rushing water
+    force_more_message += heat about you
+    force_more_message += rumble.*avalanche
+    force_more_message += rapidly growing quiet
+    # Dangerous monster arrivals
+    force_more_message += (ancient lich|lich|curse skull|orb of fire|tormentor|hellion|fiend|hell sentinel|moth of wrath).*(comes? into view|is nearby)
+    force_more_message += (shining eye|neqoxec|cacodemon|wretched star|dream sheep).*(comes? into view)
+    force_more_message += 's ghost.*(comes? into view)
     flash_screen_message += You are.* paralys
     flash_screen_message += You are.* petrif
     flash_screen_message += You fall through a shaft
     flash_screen_message += You are sucked into a shaft
+    flash_screen_message += You are confused
+    flash_screen_message += You are slowing down
+    flash_screen_message += .* grabs you
+    flash_screen_message += You are held
     monster_alert += -uniques
     monster_alert += -nasty
+
+    # Necromancer: stable spell letter assignments
+    spell_slot ^= Animate Dead:a
+    spell_slot ^= Vampiric Draining:v
+    spell_slot ^= Grave Claw:g
+    spell_slot ^= Death Channel:d
+    spell_slot ^= Fugue of the Fallen:f
+    spell_slot ^= Borgnjor's Vile Clutch:b
+    spell_slot ^= Soul Splinter:s
+    spell_slot ^= Curse of Agony:c
+    spell_slot ^= Haunt:h
+    spell_slot ^= Death's Door:j
+
+    # Necromancer: number keys cast spell slots (skip 5 -- reserved for long rest)
+    macros += M 1 za
+    macros += M 2 zv
+    macros += M 3 zg
+    macros += M 4 zd
+    macros += M 6 zf
+    macros += M 7 zb
 
     # Sound
     sound_file_path = /home/${username}/.crawl/sound/
