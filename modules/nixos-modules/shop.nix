0b07eca66da3f6e0cfc77214b69d8cd19fad7133
weegs710 <weegs@tutamail.com>
2026-08-10 16:23:50 -0400

    refactor(shop): pin nix-shop instead of vendoring it
    - .tack/pins.toml: add nix-shop, cb:weegs710/nix-shop
    - .tack/pins.lock.json: nix-shop 505d574
    - modules/nixos-modules/shop.nix: import inputs.nix-shop.nixosModules.default, programs.shop.enable + clone
    - delete lib/shop/
    - delete weegsware/shop.nix

--- modules/nixos-modules/shop.nix
+++ modules/nixos-modules/shop.nix
@@ -1,32 +1,13 @@
 {
-  pkgs,
-  lib,
+  inputs,
   config,
-  weegsware,
   ...
 }:
-let
-  # The packages output stays generic for `nix run`; only the installed copy knows where this machine's clone lives.
-  restock =
-    pkgs.runCommand "restock"
-      {
-        nativeBuildInputs = [ pkgs.makeWrapper ];
-        meta.mainProgram = "restock";
-      }
-      ''
-        makeWrapper ${weegsware.restock}/bin/restock $out/bin/restock \
-          --set SHOP_CLONE ${config.mySystem.shop.clone}
-      '';
-in
 {
-  options.mySystem.shop.clone = lib.mkOption {
-    type = lib.types.str;
-    default = "/home/${config.mySystem.user.name}/repo/nixpkgs.git";
-    description = "Bare nixpkgs clone that restock checks revisions out of";
-  };
+  imports = [ inputs.nix-shop.nixosModules.default ];
 
-  config.environment.systemPackages = [
-    weegsware.shop
-    restock
-  ];
+  programs.shop = {
+    enable = true;
+    clone = "/home/${config.mySystem.user.name}/repo/nixpkgs.git";
+  };
 }
